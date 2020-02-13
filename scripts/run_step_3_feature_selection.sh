a32a5d32b06203450e01b621ed74c40728ce8598
Miyuraj Harishchandra <miyuraj@yahoo.com>
2020-02-13 17:12:24 -0600

    run_step_3_feature_selection.sh

--- scripts/run_step_3_feature_selection.sh
+++
@@ -1,5 +0,0 @@
-/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Correlation_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
-/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Inforgain_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
-/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/InformationGainRatio_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
-/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Relief_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
-/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/SymmetricalUncert_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/s
