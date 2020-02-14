4ab03bdb4d9f40c3bf1ddb760f5f4bc2c7d902ec
Miyuraj Harishchandra <miyuraj@yahoo.com>
2020-02-13 21:48:05 -0600

    Add files via upload

--- scripts/run_step_2_FeatureSelection.sh
+++ scripts/run_step_2_FeatureSelection.sh
@@ -3,20 +3,20 @@ echo ""
 echo "Feature Selection Started."
 echo " "
 echo " "
-/usr/local/bin/python3.7 ./pipe_step_2_scripts/Correlation_Fselection.py ./pipe_step_2_input/input.csv ./pipe_step_2_output/
+/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Correlation_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
 echo " "
-/usr/local/bin/python3.7 ./pipe_step_2_scripts/Inforgain_Fselection.py ./pipe_step_2_input/input.csv ./pipe_step_2_output/
+/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Inforgain_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
 echo " "
-/usr/local/bin/python3.7 ./pipe_step_2_scripts/InformationGainRatio_Fselection.py ./pipe_step_2_input/input.csv ./pipe_step_2_output/
+/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/InformationGainRatio_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
 echo " "
-/usr/local/bin/python3.7 ./pipe_step_2_scripts/Relief_Fselection.py ./pipe_step_2_input/input.csv ./pipe_step_2_output/
+/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/Relief_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
 echo " "
-/usr/local/bin/python3.7 ./pipe_step_2_scripts/SymmetricalUncert_Fselection.py ./pipe_step_2_input/input.csv ./pipe_step_2_output/
+/usr/local/bin/python3.7 ./pipe_step_2_FS/pipe_step_2_scripts/SymmetricalUncert_Fselection.py ./pipe_step_2_FS/pipe_step_2_input/input.csv ./pipe_step_2_FS/pipe_step_2_output/
 echo " "
-scp ./pipe_step_2_output/*.csv ../pipe_step_4_clf/pipe_step_4_clf_clfers/NB
-scp ./pipe_step_2_output/*.csv ../pipe_step_4_clf/pipe_step_4_clf_clfers/SVM
-scp ./pipe_step_2_output/*.csv ../pipe_step_4_clf/pipe_step_4_clf_clfers/RF
-scp ./pipe_step_2_output/*.csv ../pipe_step_3_FAggregation/pipe_step_3_Fselected_input/
+scp ./pipe_step_2_FS/pipe_step_2_output/*.csv ./pipe_step_4_clf/pipe_step_4_clf_clfers/NB
+scp ./pipe_step_2_FS/pipe_step_2_output/*.csv ./pipe_step_4_clf/pipe_step_4_clf_clfers/SVM
+scp ./pipe_step_2_FS/pipe_step_2_output/*.csv ./pipe_step_4_clf/pipe_step_4_clf_clfers/RF
+scp ./pipe_step_2_FS/pipe_step_2_output/*.csv ./pipe_step_3_FAggregation/pipe_step_3_Fselected_input/
 
 echo ""
 echo ""
