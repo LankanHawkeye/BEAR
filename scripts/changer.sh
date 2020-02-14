8e4297711e72142a4f1346c1e10f19af97cbb4e4
Miyuraj Harishchandra <miyuraj@yahoo.com>
2020-02-14 06:37:40 -0600

    Update changer.sh

--- scripts/changer.sh
+++ scripts/changer.sh
@@ -1,3 +1,4 @@
 # This script is part of BEAR pipeline
 # This script will help the user to modify the paython interpreter locations.
-for f in *sh;do sed "s#/usr/local/bin/python3.7#$1#g" $f;done
+for f in *sh;do sed -i - e"s#/usr/local/bin/python3.7#$1#g" $f;done
+rm *e
