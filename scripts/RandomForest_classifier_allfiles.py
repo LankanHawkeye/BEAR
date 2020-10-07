306d1ce8636a66ac822044cdf9e89176d448f3a3
Miyuraj Harishchandra <miyuraj@yahoo.com>
2020-10-07 12:37:32 -0500

    Add files via upload
    Well-commented

--- scripts/RandomForest_classifier_allfiles.py
+++ scripts/RandomForest_classifier_allfiles.py
@@ -1,16 +1,19 @@
-# File created by Miyuraj Harishchandra Hikkaduwa Withanage
+################################################
+#   Part of BEAR Pipeline
+#   Created by: Miyuraj Harishchandra Hikkaduwa Withanage
+#   University of Iowa
+#   Random Forest Classifier Script
+#   bootstrap=True, n_estimators=30, random_state=45, max_features="auto",
+#                                         max_depth=1000, criterion="gini"
+##############################################
 
-print("Random Forest Classifier Started.")
+print("RF Classification Started.")
 import numpy as np
-#import matplotlib.pyplot as plt
+
 from sklearn import svm, datasets
 from sklearn.metrics import roc_curve, auc
-#from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
-#from sklearn.cross_validation import KFold
-#from sklearn.cross_validation import LeaveOneOut
-#from sklearn import cross_validation
 from sklearn import metrics
 from scipy import interp
 import csv
@@ -20,128 +23,84 @@ import re # regular expression
 from numpy import genfromtxt #to open csv file
 import pandas as pd
 import matplotlib
-#matplotlib.use('Agg')
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 
-#from sys import platform as sys_pf
-#if sys_pf == 'darwin':
-    #import matplotlib
-    #matplotlib.use("TkAgg")
+# Get the list of csv files in the working directory
 csvFiles = sorted(glob.glob('*.csv'))
-#print(csvFiles)
+# Initiating a plot
 plt.figure(1)
-#plt.plot([0, 1], [0, 1], 'k--')
+# Specifying colors used in the plot
 colors = ['red','navy','orange','dodgerblue','magenta','brown','purple','green','gold','lawngreen','aqua','gray']
-#os.remove("RF_AUC.txt")
-f = open("../../result_auc_for_each_position/RF_AUC.txt", "a")
-#color_count = 0
+# SVM_AUC Data Frame to Store AUC values
+SVM_AUC = pd.DataFrame()
+
+# Read each file in csvFiles list
 for filepicker in range(0, len(csvFiles)):
-    #color_count = color_count + 1
+    # Read the fie into dataCSV dataframe (Pandas)
     dataCSV = pd.read_csv(os.getcwd() + '/' + csvFiles[filepicker], delimiter=',',header=0)
     dataCSV_dummy = pd.get_dummies(dataCSV)
+    # Convert values of dataframe to numeric values
     dataCSV_dummy = dataCSV_dummy.to_numpy()
+    # Identify labels in dataframe
     labels = dataCSV_dummy[:, dataCSV_dummy.shape[1] - 1:dataCSV_dummy.shape[1]]
-    labels = labels.ravel()
+    # Get the feature set separated
     features = dataCSV_dummy[:, 0: dataCSV_dummy.shape[1]-1]
     SVM_list = []
+
+    # creating different feature sets by incrementing the size of feature set
     for iterator in range(1, dataCSV_dummy.shape[1]):
-        # target class column
-        # y = targets = my_data[:, my_data.shape[1] - 1:my_data.shape[1]]
-        # full dataset
-        # if topSet == 0:
-        #    topSet = dataCSV_dummy.shape[1]
+        # feature set
         X = allData = dataCSV_dummy[:, 0:iterator]
-        # binarize for four classes
-        labels = label_binarize(labels, classes=[0,1])
-        n_classes = labels.shape[1]
+        # Use leave one out cross validation
+        from sklearn.model_selection import LeaveOneOut
+        loo = LeaveOneOut()
+        loo.get_n_splits(X)
 
-        #from sklearn.model_selection import train_test_split
-        #from sklearn.model_selection import KFold
-        from sklearn.model_selection import StratifiedKFold
-
-        # if binary class, stratified Kfold needs to be used
-        nsplitting = 5
-        skf = StratifiedKFold(n_splits=nsplitting)
         random_state = 12883823
-        count = 0
-        #nsplitting = 5
-        #rf = KFold(n_splits=nsplitting, random_state=random_state)
-        # y_score is gonna be used outside loop
-        # y_score=dict()
-        # concat_y_score = float()
-        # concat_y_test = float()
-        button = False
-        A = []
-        total =0
+
         test_temp = []
-        auc_all = 0
-        for train_index, test_index in skf.split(X,labels):
-            ##print("TRAIN:", train_index, "TEST:", test_index)
+        testlabel_list = []
+        predictionoutput_list = []
+
+        # LOO-CV Procedure (train, test splitting)
+        for train_index, test_index in loo.split(X,labels):
+            # creating train set and test set (features)
             train_features, test_features = X[train_index], X[test_index]
+            # creating train set and test set (class labels)
             train_labels, test_labels = labels[train_index], labels[test_index]
-            
 
+            # importaing  RF Classifier libraries
             from sklearn.ensemble import RandomForestClassifier
-
+            # Defining RF classifier
             rf = RandomForestClassifier(bootstrap=True, n_estimators=30, random_state=45, max_features="auto",
                                         max_depth=1000, criterion="gini")
-
-            ##print("labels")
-            ##print(train_labels)
+            # Model fitting (Training)
             rf.fit(train_features, train_labels.ravel())
+
+            # Ready to calculate ROC curve and AUC
             from sklearn.metrics import roc_curve, auc
+            # Make predictions based on fitted model
             y_pred_rf = rf.predict_proba(test_features)[:, 1]
-            ##print("***********")
-            ##print(test_labels)
-            ##print("******")
+            test_labels_flat = [item for sublist in test_labels for item in sublist]
 
-            ##print(y_pred_rf)
-            total = y_pred_rf.shape[0]+total
-            count = count +1
-            # array
-            fpr_rf, tpr_rf, _ = roc_curve(test_labels, y_pred_rf)
-            roc_auc = auc(fpr_rf, tpr_rf)
-            # cannot concatenate without tolist()
-            auc_all = auc_all+roc_auc
+            # we make a list of test labels and prediction output over the crossvalidation
+            # we use this entire set to calculate the ROC and AUC
+            # Test_labels and y_pred_rf are numpy nd.array
+            testlabel_list.append(test_labels_flat)
+            predictionoutput_list.append(y_pred_rf)
 
-
-
-
-        #print("==========================================")
-        #print(auc_all)
-        average_auc = auc_all/count
-        ##print(np.asarray(A))
-        ##print(np.asarray(test_temp))
-        ##print(np.asarray(y_pred_rf).ravel())
-        ##print(np.asarray(test_labels_vector).ravel())
-        #print("=====")
-        ##print(fpr_rf_list)
-        ##print(tpr_rf_list)
-
-        SVM_list.append(average_auc)
-        #print("round complete ===============" + str(count)+ "==> AUC: " + str(average_auc))
-        iterator = iterator + 1
-
-    ##print("=================================")
-    ##print(csvFiles[filepicker])
-    f.write(csvFiles[filepicker]+"\n")
-    ##print("#printing SVM list")
-    f.write(str(SVM_list)+"\n")
-    # #print(SVM_list)
-    ##print(len(SVM_list))
+        # Calculate TPR and FPR using testlabel_list and predictionoutput_list
+        fpr_rf, tpr_rf, _ = roc_curve(np.asarray(testlabel_list), predictionoutput_list)
+        roc_auc = auc(fpr_rf, tpr_rf)
+        SVM_list.append(roc_auc)
+    # At this point we have AUC values for each csv file
+    # This is a Pandas dataframe
+    SVM_AUC[csvFiles[filepicker].replace('.csv', '')] = SVM_list
     SVM_list = np.asarray(SVM_list)
-    #print(SVM_list)
-
-    ##print("#printing X axis")
+    # Creating X Axis
     xaxis = list(range(1, dataCSV_dummy.shape[1]))
-
-    ##print(len(xaxis))
     xaxis = np.asarray(xaxis)
-    #print(xaxis)
-    #plt.figure()
-    # plt.plot([0, 1], [0, 1], 'k--')
-
     lw = 2
     # plt.plot(fpr_rf, tpr_rf, label='ROC curve of class {0} (area = {1:0.2f})' ''.format(i, roc_auc[i]))
     plt.plot(np.asarray(xaxis), np.asarray(SVM_list), color=colors[filepicker], lw=lw, label='{0}' ''.format(re.sub('.csv', '', str(csvFiles[filepicker]))))
@@ -151,10 +110,10 @@ for filepicker in range(0, len(csvFiles)):
     plt.xlabel('Ranked Features')
     plt.title('AUC (RF Classifier) VS. Ranked Feature Curve')
     plt.legend(loc="lower right")
-    plt.savefig("../../result_classifier_evalutions/RF_AUC_vs_RankedFeature_Curve.pdf")
+    plt.savefig("../../result_classifier_evalutions/RF_AUV_vs_RankedFeature_Curve.pdf")
+SVM_AUC.to_csv('../../result_auc_for_each_position/RF_AUC.csv',sep=',',index=False,header=True)
 #plt.show()
-print("Random Forest Classification Complete.")
-
+print("RF Classification Complete.")
 
 
 
